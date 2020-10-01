db89fe3b3b3a2ca8a066e854f702a66fd01b0a1d
Raghav Mehta <raghav.mehta.17@gmail.com>
2020-10-01 06:42:44 +0530

    Update app.py
    Added database connectivity

--- app.py
+++ app.py
@@ -1,82 +1,45 @@
-import os
-from flask import Flask, flash, request, redirect, url_for, jsonify, render_template, session, send_file, send_from_directory, safe_join, abort
-from werkzeug.utils import secure_filename
-import cv2
-
-from morph import CreateAffineTransform, CreateControlPoints, CreateTriangle, ResizeImage
-
-
-UPLOAD_FOLDER = './static/uploaded_images'
-OUTPUT_FOLDER = './static/output_gif'
-MORPH_FRAMES = './static/morph_frames'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_login import login_required, current_user
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
+db = SQLAlchemy()
 
-if not os.path.isdir(UPLOAD_FOLDER):
-    os.mkdir(UPLOAD_FOLDER)
-if not os.path.isdir(OUTPUT_FOLDER):
-    os.mkdir(OUTPUT_FOLDER)
-if not os.path.isdir(MORPH_FRAMES):
-    os.mkdir(MORPH_FRAMES)
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+db.init_app(app)
 
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
 
-@app.route('/api', methods=['GET'])
-def home_page():
-    return render_template("index.html")
+from models import User
 
 
-@app.route('/api/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        print(request.files)
-        if ('singleimage' not in request.files):
-            print("File not attatched!")
-            return jsonify({"File(s) attached": False})
-        file = request.files['singleimage']
-        if file.filename == '':
-            print("File Name is empty.")
-            return jsonify({"filename(s)": None})
-        if (file and allowed_file(file.filename)):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            flash('Files Uploaded', category='info')
-            print("File saved to Server...")
-            return jsonify({"Image Uploaded" : filename})
-            
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    return User.query.get(int(user_id))
 
-@app.route('/api/morph/<images>', methods=['GET'])
-def morph(images):
-    # USE IMAGE HASHED NAMES IN FUTURE
-    image_one_path = os.path.join(app.config['UPLOAD_FOLDER'], images.split("|")[0])
-    image_two_path = os.path.join(app.config['UPLOAD_FOLDER'], images.split("|")[1])
-    resize_images = ResizeImage(image_one_path, image_two_path)
-    resize_images.resizer()
-    image_one = CreateControlPoints(image_one_path)
-    image_two = CreateControlPoints(image_two_path)
-    image_one_control_pts = image_one.create_control_points()
-    image_two_control_pts = image_two.create_control_points()
-    flash('Control Points Created.', category='info')
-    del_tri = CreateTriangle(image_one_control_pts, image_two_control_pts).create_triangles()
-    flash('Delaunay Triangles Created.', category='info')
-    affine = CreateAffineTransform(del_tri, image_one_path, image_two_path, image_one_control_pts, image_two_control_pts)
-    affine.perform_affine_transform(40)
-    flash('Applied Affine Transform.', category='info')
-    try:
-        flash('Downloading Now...', category='info')
-        print("GIF Created, Initiating Download...")
-        return send_from_directory(app.config["OUTPUT_FOLDER"], filename= 'morphed.gif', as_attachment=True)
-    except (FileNotFoundError):
-        abort(404)
 
+# blueprint for auth routes in our app
+from auth import auth as auth_blueprint
+
+app.register_blueprint(auth_blueprint)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/profile', methods=['GET'])
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.name)
 
 
 if __name__ == '__main__':
-    app.config['SECRET_KEY'] = os.environ.get('SECRET') or 'hyjygjgj'
-    app.debug = True
-    app.run(threaded = True)
+    app.run(ssl_context="adhoc")
